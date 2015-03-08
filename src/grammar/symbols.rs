13341eb252a396cce3fb138aaa33194ff370cb7b
Tim Junell <tkpjun@utu.fi>
2015-03-08 12:28:35 +0200

    Day 2

--- src/grammar/symbols.rs
+++ src/grammar/symbols.rs
@@ -1,4 +1,6 @@
 pub use super::graph::Graph;
+pub use rand;
+pub use rand::Rng;
 
 #[derive(Eq, PartialEq)]
 pub enum Symbol {
@@ -20,35 +22,48 @@ pub enum Symbol {
 }
 
 pub struct Rule {
-    start: Graph<Symbol>,
-    end: Graph<Symbol>,
+    start: Vec<Box<Symbol>>,
+    end: Vec<Box<Symbol>>,
 }
 
 impl Graph<Symbol> {
 
     pub fn apply_rule(&mut self, rule: &Rule) -> bool {
+        let subs = self.find_sub_indexes(&rule.start);
+        if subs.len() == 0 {
+            return false;
+        }
+        let temp = subs.len();
+        let chosen = &subs[rand::thread_rng().gen_range(0, temp)];
 
+        true
     }
 
-    fn find_sub_indexes(&self, sub: &Graph<Symbol>) -> Option<Vec<Vec<usize>>> {
-        let mut ret = Vec::new();
-        for s_node in sub.data {
-            let mut inst = Vec::new();
-            for i in 0..self.data.len() {
-                if self.data[i].value == s_node.value {
-                    let non_matches = s_node.paths.iter().filter(|x| {
-                        self.data[i].paths.iter().any(|y| *x == y)
-                    }).collect::<Vec<&usize>>();
-                    if non_matches.len() == 0 {
-                        inst.push(i);
+    //currently reads a Room-Lock connection as Room-Lock-Room-Lock...
+    fn find_sub_indexes(&self, sub: &Vec<Box<Symbol>>) -> Vec<Vec<usize>> {
+        let mut ret: Vec<Vec<usize>> = Vec::new();
+        for i in 0..self.data.len() {
+            let mut try = Vec::new();
+            if self.data[i].value == *sub[0] {
+                try.push(i);
+                ret.push(try);
+            }
+        }
+        if sub.len() == 1 {
+            return ret;
+        }
+        //for every remaining node of the linear subgraph
+        for index in 1..sub.len() {
+            for vec in &mut ret {
+                'inner: for path in &self.data[vec[index - 1]].paths {
+                    if self.data[*path].value == *sub[index] {
+                        vec.push(*path);
+                        break 'inner;
                     }
                 }
             }
-            if inst.len() > 0 {
-                ret.push(inst);
-            }
-            else { return None; }
+            ret = ret.into_iter().filter(|x| x.len() == index + 1).collect::<Vec<Vec<usize>>>();
         }
-        Some(ret)
+        return ret;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate rand;
+
 mod grammar {
     mod symbols;
     mod rules;
