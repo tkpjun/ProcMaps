1eedaf3ee161294ebcd019330b2502ab7adb4b47
Tim Junell <tkpjun@outlook.com>
2015-03-14 10:54:28 +0200

    Actually finalized Rule

--- src/bin/start.rs
+++ src/bin/start.rs
@@ -4,27 +4,27 @@ pub use proc_maps::mission_graph::graph;
 pub use proc_maps::mission_graph::grammar;
 pub use proc_maps::mission_graph::grammar::Symbol;
 pub use proc_maps::mission_graph::grammar::PathType;
+pub use proc_maps::mission_graph::grammar::Anchor;
 
 fn main() {
     let mut graph = graph::Graph::new();
     graph.push_node(Symbol::LevelEntr);
+    graph.push_node(Symbol::Combat(0));
     graph.push_node(Symbol::LevelExit);
     graph.add_path(0, 1);
+    graph.add_path(1, 2);
     println!("Before:\n{}", graph.to_string());
 
     let s = vec![Symbol::LevelEntr, Symbol::LevelExit];
+    let p = vec![PathType::Loose];
     let mut e = graph::Graph::new();
     e.push_node(Symbol::LevelEntr);
     e.push_node(Symbol::Key(0));
     e.push_node(Symbol::KeyDoor(vec![0]));
-    e.push_node(Symbol::LevelExit);
     e.add_path(0, 1);
     e.add_path(1, 2);
-    e.add_path(2, 3);
-    let p = vec![PathType::Tight];
-    let rule = grammar::Rule{ start:s, s_paths:p, result:e, anchor:((0, 0),(1, 3))};
+    let rule = grammar::Rule{ start:s, s_paths:p, result:e, anchor:((0, 0),(1, Anchor::Connected(2)))};
 
-    //graph.apply_rule(rule);
     graph.apply_rule(&rule);
     println!("After:\n{}", graph.to_string());
 }
--- src/mission_graph/grammar.rs
+++ src/mission_graph/grammar.rs
@@ -1,8 +1,9 @@
-pub use super::graph;
-pub use super::graph::Graph;
-pub use rand;
-pub use rand::Rng;
-pub use std::collections::linked_list::LinkedList;
+use super::graph;
+use super::graph::Graph;
+use rand;
+use rand::Rng;
+use std::collections::linked_list::LinkedList;
+use std::num::SignedInt;
 
 #[derive(Eq, PartialEq, Debug, Clone)]
 pub enum Symbol {
@@ -35,12 +36,17 @@ pub struct Rule {
     //0: Index of a node in the start graph and its position in the result graph.
     //1: Offset of a 2nd node; 0 means only one anchor node.
     //Nodes between the two anchors in the original graph are removed.
-    pub anchor: ((usize, usize), (isize, usize)),
+    pub anchor: ((usize, usize), (isize, Anchor)),
 }
 
 pub enum PathType {
     Tight,
     Loose,
+}
+
+pub enum Anchor {
+    Is(usize),
+    Connected(usize),
 }
 
 impl Graph<Symbol> {
@@ -54,48 +60,89 @@ impl Graph<Symbol> {
         let random = rand::thread_rng().gen_range(0, len);
         let &(ref sub_i, ref sub_i_i) = &subs[random];
         let len = self.data.len();
-        if sub_i.len() == 2 {
-            self.remove_path(sub_i[0], sub_i[1]);
-        }
         let n1_s = sub_i_i[(rule.anchor.0).0];
         let n1_e = (rule.anchor.0).1;
         let n2_s = (sub_i_i[(rule.anchor.0).0] as isize + (rule.anchor.1).0) as usize;
-        let n2_e = (rule.anchor.1).1;
+        let n2_in_res = match (rule.anchor.1).1 {
+            Anchor::Is(_) => true,
+            _ => false
+        };
+        let n2_e = match (rule.anchor.1).1 {
+            Anchor::Is(a) => a,
+            Anchor::Connected(a) => a
+        };
 
+        if sub_i.len() == 2 {
+            self.remove_path(sub_i[0], sub_i[1]);
+        }
+        else if (rule.anchor.1).0.abs() == 1 {
+            self.remove_path(sub_i[n1_s], sub_i[n2_s]);
+        }
         let mut anchors_passed_over = 0;
         //Add the end graph sans anchors to self, update anchors
         for index in 0..rule.result.data.len() {
-            if n1_e != index && n2_e != index {
-                let node = &rule.result.data[index];
-                let paths = node.paths.iter().map(|a|{
-                    if n1_e == *a {
-                        sub_i[n1_s]
-                    }
-                    else if n2_e == *a {
-                        sub_i[n2_s]
-                    }
-                    else {
-                        a + len - anchors_passed_over
-                    }
-                }).collect::<Vec<usize>>();
-                self.push_node(node.value.clone());
-                let last = self.data.len() - 1;
-                self.set_paths(last, &paths);
-            }
-            else {
-                let a_pair = if n1_e == index {
-                    (n1_s, n1_e)
+            if n2_in_res {
+                if n1_e != index && n2_e != index {
+                    let node = &rule.result.data[index];
+                    let paths = node.paths.iter().map(|a|{
+                        if n1_e == *a {
+                            sub_i[n1_s]
+                        }
+                        else if n2_e == *a {
+                            sub_i[n2_s]
+                        }
+                        else {
+                            a + len - anchors_passed_over
+                        }
+                    }).collect::<Vec<usize>>();
+                    self.push_node(node.value.clone());
+                    let last = self.data.len() - 1;
+                    self.set_paths(last, &paths);
                 }
                 else {
-                    (n2_s, n2_e)
-                };
-                let node = &rule.result.data[a_pair.1];
-                self.data[sub_i[a_pair.0]].value = node.value.clone();
-                let mut res_paths = node.paths.iter().map(|a|{
-                    *a + len - 1//BUG: Minus anchors passed over when the data point was added
-                }).collect::<LinkedList<usize>>();
-                self.data[sub_i[a_pair.0]].paths.append(&mut res_paths);
-                anchors_passed_over += 1;
+                    let a_pair = if n1_e == index {
+                        (n1_s, n1_e)
+                    }
+                    else {
+                        (n2_s, n2_e)
+                    };
+                    let node = &rule.result.data[a_pair.1];
+                    self.data[sub_i[a_pair.0]].value = node.value.clone();
+                    let mut res_paths = node.paths.iter().map(|a|{
+                        *a + len - 1//BUG: Minus anchors passed over when the data point was added
+                    }).collect::<LinkedList<usize>>();
+                    self.data[sub_i[a_pair.0]].paths.append(&mut res_paths);
+                    anchors_passed_over += 1;
+                }
+            }
+            else {
+                if n1_e != index {
+                    let node = &rule.result.data[index];
+                    let paths = node.paths.iter().map(|a|{
+                        if n1_e == *a {
+                            sub_i[n1_s]
+                        }
+                        else {
+                            a + len - anchors_passed_over
+                        }
+                    }).collect::<Vec<usize>>();
+                    self.push_node(node.value.clone());
+                    let last = self.data.len() - 1;
+                    self.set_paths(last, &paths);
+                    if index == n2_e{
+                        self.add_path(last, n2_s);
+                    }
+                }
+                else {
+                    let a_pair = (n1_s, n1_e);
+                    let node = &rule.result.data[a_pair.1];
+                    self.data[sub_i[a_pair.0]].value = node.value.clone();
+                    let mut res_paths = node.paths.iter().map(|a|{
+                        *a + len - 1//BUG: Minus anchors passed over when the data point was added
+                    }).collect::<LinkedList<usize>>();
+                    self.data[sub_i[a_pair.0]].paths.append(&mut res_paths);
+                    anchors_passed_over += 1;
+                }
             }
         }
 
